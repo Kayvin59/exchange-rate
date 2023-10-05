749d8ac1dda21d0e7a95dd3cd5b5aaa3bcab9e4e
Kayvin <kayvin4@hotmail.com>
2023-10-05 10:04:35 +0200

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-3xl font-bold">Exchange rate between BTC & USDT</h1>
+    </main>
+  )
+}
